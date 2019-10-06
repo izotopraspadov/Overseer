8f56c596b2763a9d61cf1f36ec766326c6af9fa4
Sergey Simonov <izotopraspadov@gmail.com>
2019-10-06 12:53:04 +0300

    added for pom.xml: dependenciesManager & build

--- src/test/java/edu/guap/enclave/repository/jpa/RegionRepositoryImplTest.java
+++ src/test/java/edu/guap/enclave/repository/jpa/RegionRepositoryImplTest.java
@@ -1,10 +1,26 @@
 package edu.guap.enclave.repository.jpa;
 
+import edu.guap.enclave.repository.RegionRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@SpringJUnitConfig(locations = {
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Sql(scripts = "classpath:db/POPULATE_DB.sql", config = @SqlConfig(encoding = "UTF-8"))
 class RegionRepositoryImplTest {
+
+    @Autowired
+    private RegionRepository regionRepository;
 
     @Test
     void save() {
@@ -20,5 +36,6 @@ class RegionRepositoryImplTest {
 
     @Test
     void getAll() {
+        regionRepository.getAll().forEach(System.out::println);
     }
 }
