f47e0c589f6f5a890a05a1eef1554fdf3ab09dd3
Sergey Simonov <izotopraspadov@gmail.com>
2019-10-06 12:37:37 +0300

    added dependencies: jUnit and other by testing

---
+++ src/test/java/edu/guap/enclave/repository/jpa/RegionRepositoryImplTest.java
@@ -0,0 +1,24 @@
+package edu.guap.enclave.repository.jpa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegionRepositoryImplTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
