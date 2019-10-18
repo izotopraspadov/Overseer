79bf618729ed0873523143098dd37c5ded178f8c
Sergey Simonov <izotopraspadov@gmail.com>
2019-10-18 09:49:14 +0300

    changed Employee

--- src/main/java/edu/guap/enclave/model/Employee.java
+++ src/main/java/edu/guap/enclave/model/Employee.java
@@ -5,19 +5,34 @@ import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity
-@Table(name = "employees")
+@Table(name = "employees", uniqueConstraints = {@UniqueConstraint(columnNames = "login", name = "employees_unique_login_idx")})
 public class Employee extends AbstractFullNameEntity {
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "region_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @NotNull
     private Region region;
+
+    @Column(name = "login", nullable = false, unique = true)
+    @NotBlank
+    @Size(max = 255)
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    @NotBlank
+    @Size(max = 255)
+    private String password;
+
+    @Column(name = "address")
+    @Size(max = 255)
+    private String address;
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "employee")
     @OrderBy("number DESC")
@@ -27,19 +42,18 @@ public class Employee extends AbstractFullNameEntity {
     @OrderBy("email DESC")
     private List<EmployeeEmail> emails;
 
-    @Column(name = "address")
-    @Size(min = 10, max = 255)
-    private String address;
-
     public Employee() {
     }
 
-    public Employee(Integer id, String fullName, Region region, List<EmployeePhone> phones, List<EmployeeEmail> emails, String address) {
+    public Employee(Integer id, String fullName, Region region, String address, String login,
+                    String password, List<EmployeePhone> phones, List<EmployeeEmail> emails) {
         super(id, fullName);
         this.region = region;
+        this.address = address;
+        this.login = login;
+        this.password = password;
         this.phones = phones;
         this.emails = emails;
-        this.address = address;
     }
 
     public Region getRegion() {
@@ -48,6 +62,30 @@ public class Employee extends AbstractFullNameEntity {
 
     public void setRegion(Region region) {
         this.region = region;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public List<EmployeePhone> getPhones() {
@@ -66,21 +104,17 @@ public class Employee extends AbstractFullNameEntity {
         this.emails = emails;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     @Override
     public String toString() {
         return "Employee{" +
                 "id=" + id +
-                ", fullName='" + fullName + '\'' +
                 ", region=" + region +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
                 ", address='" + address + '\'' +
+                ", phones=" + phones +
+                ", emails=" + emails +
+                ", fullName='" + fullName + '\'' +
                 '}';
     }
 }
