f3d63867ca0f66caf79162c42489572c32ca113b
Sergey Simonov <izotopraspadov@gmail.com>
2019-11-15 13:28:33 +0300

    BaseTo, UserTo: created

---
+++ src/main/java/edu/guap/enclave/to/BaseTo.java
@@ -0,0 +1,24 @@
+package edu.guap.enclave.to;
+
+import edu.guap.enclave.model.abstract_entities.HasId;
+
+public abstract class BaseTo implements HasId {
+    protected Integer id;
+
+    public BaseTo() {
+    }
+
+    public BaseTo(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
