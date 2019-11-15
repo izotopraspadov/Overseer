3949e2e77ac9dbb53f50ca97bef2b22cc0d2a8b5
Sergey Simonov <izotopraspadov@gmail.com>
2019-11-15 13:27:55 +0300

    Role: edit for Security

--- src/main/java/edu/guap/enclave/model/Role.java
+++ src/main/java/edu/guap/enclave/model/Role.java
@@ -1,6 +1,14 @@
 package edu.guap.enclave.model;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     USER,
-    ADMIN
+    ADMIN;
+
+
+    @Override
+    public String getAuthority() {
+        return null;
+    }
 }
