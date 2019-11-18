757de60193692fc4232d6193b38da14ae55677c1
Sergey Simonov <izotopraspadov@gmail.com>
2019-11-18 11:27:58 +0300

    AdminEmployeeController: changed update method

--- src/main/java/edu/guap/enclave/web/servlets/AdminEmployeeController.java
+++ src/main/java/edu/guap/enclave/web/servlets/AdminEmployeeController.java
@@ -3,8 +3,10 @@ package edu.guap.enclave.web.servlets;
 import edu.guap.enclave.model.Email;
 import edu.guap.enclave.model.Employee;
 import edu.guap.enclave.model.Phone;
+import edu.guap.enclave.model.Region;
 import edu.guap.enclave.repository.EmployeeRepository;
 import edu.guap.enclave.repository.RegionRepository;
+import edu.guap.enclave.util.ValidationUtil;
 import edu.guap.enclave.web.AbstractEmployeeController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,11 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
-import java.net.http.HttpRequest;
-import java.util.Collections;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = AdminEmployeeController.URL)
@@ -35,11 +34,11 @@ public class AdminEmployeeController extends AbstractEmployeeController {
     public String get(ModelMap model, @PathVariable("id") int id) {
         Employee employee = super.getWithSalaryAndPhonesAndEmails(id).orElse(null);
         model.addAttribute("employee", employee);
-        return "employee";
+        return "employeeProfile";
     }
+
     @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public String update(ModelMap model,
-                         @RequestParam() String id,
+    public String update(@RequestParam() String id,
                          @RequestParam() String fullName,
                          @RequestParam() String password,
                          @RequestParam() String login,
@@ -59,20 +58,26 @@ public class AdminEmployeeController extends AbstractEmployeeController {
         employee.setPassword(password);
         employee.setLogin(login);
         employee.setAddress(address);
-        employee.setRegion(region.equals(employee.getRegion().getTitle()) ? employee.getRegion() : regionRepository.getByTitle(region).get());
+        if (!region.equals(employee.getRegion().getTitle()))
+            regionRepository.getByTitle(region).ifPresent(employee::setRegion);
+
         String[] phones = {phone1, phone2, phone3};
         int count = 0;
-        for (Phone phone: employee.getPhones()) {
-            if (phones[count] != null && !phone.getNumber().equals(phones[count])) phone.setNumber(phones[count]);
+        for (Phone phone : employee.getPhones()) {
+            if (phones[count] != null
+                    && !phone.getNumber().equals(phones[count])
+                    && ValidationUtil.isValidPhoneLength(phones[count]))
+                phone.setNumber(phones[count]);
             count++;
         }
         String[] emails = {email1, email2, email3};
         count = 0;
-        for (Email email: employee.getEmails()) {
+        for (Email email : employee.getEmails()) {
             if (emails[count] != null && !email.getEmail().equals(emails[count])) email.setEmail(emails[count]);
             count++;
         }
-        employee.getSalary().stream().findFirst().get().setAmount(new BigDecimal(salary));
+        if (ValidationUtil.isBigDecimal(salary))
+            employee.getSalary().stream().findFirst().get().setAmount(new BigDecimal(salary));
 
         repository.save(employee, employee.getRegion().getId());
 
