cad88a3086c05ed13d69b83bed90d9440feb1cde
izotopraspadov <izotopraspadov@gmail.com>
2020-03-25 08:11:22 +0300

    renamed sql scripts

--- src/test/java/edu/born/overseer/repository/jpa/RegionRepositoryImplTest.java
+++ src/test/java/edu/born/overseer/repository/jpa/RegionRepositoryImplTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
         "classpath:spring/spring-db.xml"
 })
 @RunWith(SpringJUnit4ClassRunner.class)
-@Sql(scripts = "classpath:db/POPULATE_DB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@Sql(scripts = "classpath:db/population.sql", config = @SqlConfig(encoding = "UTF-8"))
 class RegionRepositoryImplTest {
 
     @Autowired
