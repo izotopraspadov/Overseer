624e6ddcc0e6285f19e723440ea7fd9d628707dc
izotopraspadov <izotopraspadov@gmail.com>
2020-03-25 07:52:49 +0300

    remove heroku prop

--- hr.bat
+++
@@ -1,3 +0,0 @@
-call mvn -B -s settings.xml -DskipTests=true clean package
-rem call java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/topjava" -jar target/dependency/webapp-runner.jar target/*.war
-call java  -DDATABASE_URL="postgres://user:password@localhost:5432/enclave" -classpath target/dependency webapp.runner.launch.Main target/*.war
