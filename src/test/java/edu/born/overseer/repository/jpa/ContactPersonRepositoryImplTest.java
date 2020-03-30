1271aa0c15719c95a8132d8be120cacd16cc7a8c
izotopraspadov <izotopraspadov@gmail.com>
2020-03-30 10:09:56 +0300

    created test ContactPersonRepositoryImplTest.create

--- src/test/java/edu/born/overseer/repository/jpa/ContactPersonRepositoryImplTest.java
+++ src/test/java/edu/born/overseer/repository/jpa/ContactPersonRepositoryImplTest.java
@@ -9,6 +9,11 @@ import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static edu.born.overseer.ContactPersonTestData.getPreparedCreate;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 @SpringJUnitConfig(locations = {"classpath:spring/spring-db.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 @Sql(scripts = "classpath:db/population.sql", config = @SqlConfig(encoding = "UTF-8"))
@@ -19,6 +24,12 @@ class ContactPersonRepositoryImplTest {
 
     @Test
     void create() {
+        var prepared = getPreparedCreate();
+        var savedId = contactPersonRepository
+                .save(prepared, prepared.getCompany().getId())
+                .getId();
+        prepared.setId(savedId);
+        assertThat(contactPersonRepository.getById(savedId), is(equalTo(prepared)));
     }
 
     @Test
@@ -52,4 +63,5 @@ class ContactPersonRepositoryImplTest {
     @Test
     void getAllByCompany() {
     }
+
 }
