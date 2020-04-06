bbf69229476762bfbc70dea7df65434877a5eeae
izotopraspadov <izotopraspadov@gmail.com>
2020-04-06 15:25:27 +0300

    PaymentFormat: overload methods

--- src/main/java/edu/born/overseer/annotation/PaymentFormat.java
+++ src/main/java/edu/born/overseer/annotation/PaymentFormat.java
@@ -1,6 +1,7 @@
 package edu.born.overseer.annotation;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
@@ -8,4 +9,7 @@ import java.lang.annotation.*;
 @Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PaymentFormat {
+    String message() default "Invalid payment format";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
