a302ff9762517b684d6c3ec5faf31140772526b8
izotopraspadov <izotopraspadov@gmail.com>
2020-04-06 12:26:17 +0300

    removed readable values by CounterpartyType

--- src/main/java/edu/born/overseer/model/CounterpartyType.java
+++ src/main/java/edu/born/overseer/model/CounterpartyType.java
@@ -1,16 +1,6 @@
 package edu.born.overseer.model;
 
 public enum CounterpartyType {
-    COMPANY("Компания"),
-    EMPLOYEE("Сотрудник");
-
-    private String description;
-
-    CounterpartyType(String description) {
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
+    COMPANY,
+    EMPLOYEE
 }
