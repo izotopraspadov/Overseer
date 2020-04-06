388404412a5c9c86d684290495aaf1df363591f7
izotopraspadov <izotopraspadov@gmail.com>
2020-04-06 16:12:12 +0300

    changed PhoneTestData: fix valid phone numbers

--- src/test/java/edu/born/overseer/PhoneTestData.java
+++ src/test/java/edu/born/overseer/PhoneTestData.java
@@ -30,31 +30,31 @@ public class PhoneTestData {
     public static final int NEW_CONTACT_PERSON_PHONE_13_ID = START_SEQUENCE + 131;
 
     public static final Phone CONTACT_PERSON_PHONE_1 =
-            new Phone(CONTACT_PERSON_PHONE_1_ID, CONTACT_PERSON_1, CONTACT_PERSON, "00000000001");
+            new Phone(CONTACT_PERSON_PHONE_1_ID, CONTACT_PERSON_1, CONTACT_PERSON, "+7-000-000-00-01");
     public static final Phone CONTACT_PERSON_PHONE_2 =
-            new Phone(CONTACT_PERSON_PHONE_2_ID, CONTACT_PERSON_1, CONTACT_PERSON, "00000000002");
+            new Phone(CONTACT_PERSON_PHONE_2_ID, CONTACT_PERSON_1, CONTACT_PERSON, "+7-000-000-00-02");
     public static final Phone CONTACT_PERSON_PHONE_3 =
-            new Phone(CONTACT_PERSON_PHONE_3_ID, CONTACT_PERSON_2, CONTACT_PERSON, "00000000003");
+            new Phone(CONTACT_PERSON_PHONE_3_ID, CONTACT_PERSON_2, CONTACT_PERSON, "+7-000-000-00-03");
     public static final Phone CONTACT_PERSON_PHONE_4 =
-            new Phone(CONTACT_PERSON_PHONE_4_ID, CONTACT_PERSON_3, CONTACT_PERSON, "00000000004");
+            new Phone(CONTACT_PERSON_PHONE_4_ID, CONTACT_PERSON_3, CONTACT_PERSON, "+7-000-000-00-04");
     public static final Phone CONTACT_PERSON_PHONE_5 =
-            new Phone(CONTACT_PERSON_PHONE_5_ID, CONTACT_PERSON_3, CONTACT_PERSON, "00000000005");
+            new Phone(CONTACT_PERSON_PHONE_5_ID, CONTACT_PERSON_3, CONTACT_PERSON, "+7-000-000-00-05");
     public static final Phone CONTACT_PERSON_PHONE_6 =
-            new Phone(CONTACT_PERSON_PHONE_6_ID, CONTACT_PERSON_3, CONTACT_PERSON, "00000000006");
+            new Phone(CONTACT_PERSON_PHONE_6_ID, CONTACT_PERSON_3, CONTACT_PERSON, "+7-000-000-00-06");
     public static final Phone CONTACT_PERSON_PHONE_7 =
-            new Phone(CONTACT_PERSON_PHONE_7_ID, CONTACT_PERSON_4, CONTACT_PERSON, "00000000007");
+            new Phone(CONTACT_PERSON_PHONE_7_ID, CONTACT_PERSON_4, CONTACT_PERSON, "+7-000-000-00-07");
     public static final Phone CONTACT_PERSON_PHONE_8 =
-            new Phone(CONTACT_PERSON_PHONE_8_ID, CONTACT_PERSON_4, CONTACT_PERSON, "00000000008");
+            new Phone(CONTACT_PERSON_PHONE_8_ID, CONTACT_PERSON_4, CONTACT_PERSON, "+7-000-000-00-08");
     public static final Phone CONTACT_PERSON_PHONE_9 =
-            new Phone(CONTACT_PERSON_PHONE_9_ID, CONTACT_PERSON_5, CONTACT_PERSON, "00000000009");
+            new Phone(CONTACT_PERSON_PHONE_9_ID, CONTACT_PERSON_5, CONTACT_PERSON, "+7-000-000-00-09");
     public static final Phone CONTACT_PERSON_PHONE_10 =
-            new Phone(CONTACT_PERSON_PHONE_10_ID, CONTACT_PERSON_6, CONTACT_PERSON, "00000000010");
+            new Phone(CONTACT_PERSON_PHONE_10_ID, CONTACT_PERSON_6, CONTACT_PERSON, "+7-000-000-00-10");
     public static final Phone CONTACT_PERSON_PHONE_11 =
-            new Phone(CONTACT_PERSON_PHONE_11_ID, CONTACT_PERSON_7, CONTACT_PERSON, "00000000011");
+            new Phone(CONTACT_PERSON_PHONE_11_ID, CONTACT_PERSON_7, CONTACT_PERSON, "+7-000-000-00-11");
     public static final Phone CONTACT_PERSON_PHONE_12 =
-            new Phone(CONTACT_PERSON_PHONE_12_ID, CONTACT_PERSON_7, CONTACT_PERSON, "00000000012");
+            new Phone(CONTACT_PERSON_PHONE_12_ID, CONTACT_PERSON_7, CONTACT_PERSON, "+7-000-000-00-12");
     public static final Phone CONTACT_PERSON_PHONE_13 =
-            new Phone(CONTACT_PERSON_PHONE_13_ID, CONTACT_PERSON_8, CONTACT_PERSON, "00000000013");
+            new Phone(CONTACT_PERSON_PHONE_13_ID, CONTACT_PERSON_8, CONTACT_PERSON, "+7-000-000-00-13");
 
     public static final Set<Phone> CONTACT_PERSON_1_PHONES = Set.of(CONTACT_PERSON_PHONE_1, CONTACT_PERSON_PHONE_2);
     public static final Set<Phone> CONTACT_PERSON_2_PHONES = Set.of(CONTACT_PERSON_PHONE_3);
@@ -66,12 +66,12 @@ public class PhoneTestData {
     public static final Set<Phone> CONTACT_PERSON_8_PHONES = Set.of(CONTACT_PERSON_PHONE_13);
 
     public static Set<Phone> getPreparedCreatedSet(ContactPerson owner) {
-        return Set.of(new Phone(null, owner, CONTACT_PERSON, "00000000200"));
+        return Set.of(new Phone(null, owner, CONTACT_PERSON, "+7-000-000-99-99"));
     }
 
     public static Set<Phone> getPreparedUpdateSet(ContactPerson owner) {
-        return Set.of(new Phone(CONTACT_PERSON_PHONE_1_ID, owner, CONTACT_PERSON, "00000000999"),
-                new Phone(CONTACT_PERSON_PHONE_2_ID, owner, CONTACT_PERSON, "00000000998"));
+        return Set.of(new Phone(CONTACT_PERSON_PHONE_1_ID, owner, CONTACT_PERSON, "+7-000-000-99-97"),
+                new Phone(CONTACT_PERSON_PHONE_2_ID, owner, CONTACT_PERSON, "+7-000-000-99-98"));
     }
 
 }
