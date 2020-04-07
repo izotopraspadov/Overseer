f0a04a766601720e842289b91b572a0eeaab0b16
izotopraspadov <izotopraspadov@gmail.com>
2020-04-07 13:32:02 +0300

    PhoneTestData: added all for employees

--- src/test/java/edu/born/overseer/PhoneTestData.java
+++ src/test/java/edu/born/overseer/PhoneTestData.java
@@ -1,12 +1,15 @@
 package edu.born.overseer;
 
 import edu.born.overseer.model.ContactPerson;
+import edu.born.overseer.model.Employee;
 import edu.born.overseer.model.Phone;
 
 import java.util.Set;
 
 import static edu.born.overseer.ContactPersonTestData.*;
+import static edu.born.overseer.EmployeeTestData.*;
 import static edu.born.overseer.model.OwnerType.CONTACT_PERSON;
+import static edu.born.overseer.model.OwnerType.EMPLOYEE;
 import static edu.born.overseer.model.abstraction.AbstractBaseEntity.START_SEQUENCE;
 
 public class PhoneTestData {
@@ -27,7 +30,16 @@ public class PhoneTestData {
     public static final int CONTACT_PERSON_PHONE_12_ID = START_SEQUENCE + 67;
     public static final int CONTACT_PERSON_PHONE_13_ID = START_SEQUENCE + 68;
 
-    public static final int NEW_CONTACT_PERSON_PHONE_13_ID = START_SEQUENCE + 131;
+    public static final int EMPLOYEE_PHONE_1_ID = START_SEQUENCE + 69;
+    public static final int EMPLOYEE_PHONE_2_ID = START_SEQUENCE + 70;
+    public static final int EMPLOYEE_PHONE_3_ID = START_SEQUENCE + 71;
+    public static final int EMPLOYEE_PHONE_4_ID = START_SEQUENCE + 72;
+    public static final int EMPLOYEE_PHONE_5_ID = START_SEQUENCE + 73;
+    public static final int EMPLOYEE_PHONE_6_ID = START_SEQUENCE + 74;
+    public static final int EMPLOYEE_PHONE_7_ID = START_SEQUENCE + 75;
+    public static final int EMPLOYEE_PHONE_8_ID = START_SEQUENCE + 76;
+    public static final int EMPLOYEE_PHONE_9_ID = START_SEQUENCE + 77;
+    public static final int EMPLOYEE_PHONE_10_ID = START_SEQUENCE + 78;
 
     public static final Phone CONTACT_PERSON_PHONE_1 =
             new Phone(CONTACT_PERSON_PHONE_1_ID, CONTACT_PERSON_1, CONTACT_PERSON, "+7-000-000-00-01");
@@ -56,6 +68,27 @@ public class PhoneTestData {
     public static final Phone CONTACT_PERSON_PHONE_13 =
             new Phone(CONTACT_PERSON_PHONE_13_ID, CONTACT_PERSON_8, CONTACT_PERSON, "+7-000-000-00-13");
 
+    public static final Phone EMPLOYEE_PHONE_1 =
+            new Phone(EMPLOYEE_PHONE_1_ID, EMPLOYEE_1, EMPLOYEE, "+7-000-000-00-14");
+    public static final Phone EMPLOYEE_PHONE_2 =
+            new Phone(EMPLOYEE_PHONE_2_ID, EMPLOYEE_1, EMPLOYEE, "+7-000-000-00-15");
+    public static final Phone EMPLOYEE_PHONE_3 =
+            new Phone(EMPLOYEE_PHONE_3_ID, EMPLOYEE_1, EMPLOYEE, "+7-000-000-00-16");
+    public static final Phone EMPLOYEE_PHONE_4 =
+            new Phone(EMPLOYEE_PHONE_4_ID, EMPLOYEE_2, EMPLOYEE, "+7-000-000-00-17");
+    public static final Phone EMPLOYEE_PHONE_5 =
+            new Phone(EMPLOYEE_PHONE_5_ID, EMPLOYEE_3, EMPLOYEE, "+7-000-000-00-18");
+    public static final Phone EMPLOYEE_PHONE_6 =
+            new Phone(EMPLOYEE_PHONE_6_ID, EMPLOYEE_4, EMPLOYEE, "+7-000-000-00-19");
+    public static final Phone EMPLOYEE_PHONE_7 =
+            new Phone(EMPLOYEE_PHONE_7_ID, EMPLOYEE_4, EMPLOYEE, "+7-000-000-00-20");
+    public static final Phone EMPLOYEE_PHONE_8 =
+            new Phone(EMPLOYEE_PHONE_8_ID, EMPLOYEE_5, EMPLOYEE, "+7-000-000-00-21");
+    public static final Phone EMPLOYEE_PHONE_9 =
+            new Phone(EMPLOYEE_PHONE_9_ID, EMPLOYEE_6, EMPLOYEE, "+7-000-000-00-22");
+    public static final Phone EMPLOYEE_PHONE_10 =
+            new Phone(EMPLOYEE_PHONE_10_ID, EMPLOYEE_6, EMPLOYEE, "+7-000-000-00-23");
+
     public static final Set<Phone> CONTACT_PERSON_1_PHONES = Set.of(CONTACT_PERSON_PHONE_1, CONTACT_PERSON_PHONE_2);
     public static final Set<Phone> CONTACT_PERSON_2_PHONES = Set.of(CONTACT_PERSON_PHONE_3);
     public static final Set<Phone> CONTACT_PERSON_3_PHONES = Set.of(CONTACT_PERSON_PHONE_4, CONTACT_PERSON_PHONE_5, CONTACT_PERSON_PHONE_6);
@@ -64,6 +97,13 @@ public class PhoneTestData {
     public static final Set<Phone> CONTACT_PERSON_6_PHONES = Set.of(CONTACT_PERSON_PHONE_10);
     public static final Set<Phone> CONTACT_PERSON_7_PHONES = Set.of(CONTACT_PERSON_PHONE_11, CONTACT_PERSON_PHONE_12);
     public static final Set<Phone> CONTACT_PERSON_8_PHONES = Set.of(CONTACT_PERSON_PHONE_13);
+
+    public static final Set<Phone> EMPLOYEE_1_PHONES = Set.of(EMPLOYEE_PHONE_1, EMPLOYEE_PHONE_2, EMPLOYEE_PHONE_3);
+    public static final Set<Phone> EMPLOYEE_2_PHONES = Set.of(EMPLOYEE_PHONE_4);
+    public static final Set<Phone> EMPLOYEE_3_PHONES = Set.of(EMPLOYEE_PHONE_5);
+    public static final Set<Phone> EMPLOYEE_4_PHONES = Set.of(EMPLOYEE_PHONE_6, EMPLOYEE_PHONE_7);
+    public static final Set<Phone> EMPLOYEE_5_PHONES = Set.of(EMPLOYEE_PHONE_8);
+    public static final Set<Phone> EMPLOYEE_6_PHONES = Set.of(EMPLOYEE_PHONE_9, EMPLOYEE_PHONE_10);
 
     public static Set<Phone> getPreparedCreatedSet(ContactPerson owner) {
         return Set.of(new Phone(null, owner, CONTACT_PERSON, "+7-000-000-99-99"));
@@ -76,6 +116,20 @@ public class PhoneTestData {
 
     public static Set<Phone> getPreparedInvalidSet(ContactPerson owner) {
         return Set.of(new Phone(null, owner, CONTACT_PERSON, "0000-000-9999"));
+    }
+
+    public static Set<Phone> getPreparedCreatedSet(Employee owner) {
+        return Set.of(new Phone(null, owner, EMPLOYEE, "+7-000-000-99-99"));
+    }
+
+    public static Set<Phone> getPreparedUpdateSet(Employee owner) {
+        return Set.of(new Phone(EMPLOYEE_PHONE_1_ID, owner, EMPLOYEE, "+7-000-000-99-96"),
+                new Phone(EMPLOYEE_PHONE_2_ID, owner, EMPLOYEE, "+7-000-000-99-97"),
+                new Phone(EMPLOYEE_PHONE_3_ID, owner, EMPLOYEE, "+7-000-000-99-98"));
+    }
+
+    public static Set<Phone> getPreparedInvalidSet(Employee owner) {
+        return Set.of(new Phone(null, owner, EMPLOYEE, "0000-000-9999"));
     }
 
 }
