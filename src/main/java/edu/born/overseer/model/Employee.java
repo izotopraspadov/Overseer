a614926f7214f21e06389a557ff67b3cd0e399c1
izotopraspadov <izotopraspadov@gmail.com>
2020-05-12 18:19:24 +0300

    Employee: added Fluent API

--- src/main/java/edu/born/overseer/model/Employee.java
+++ src/main/java/edu/born/overseer/model/Employee.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -46,151 +47,226 @@ import java.util.Set;
 })
 public class Employee extends AbstractFullNameEntity {
 
-    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "region_id", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
     @NotNull
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "region_id", nullable = false)
     private Region region;
 
-    @Column(name = "login", nullable = false, unique = true)
     @NotBlank
     @Size(max = 255)
+    @Column(name = "login", nullable = false, unique = true)
     private String login;
 
-    @Column(name = "password", nullable = false)
     @NotBlank
     @Size(max = 255)
+    @Column(name = "password", nullable = false)
     private String password;
 
-    @Column(name = "address")
     @Size(max = 255)
+    @Column(name = "address")
     private String address;
 
     @Enumerated(EnumType.STRING)
+    @BatchSize(size = 200)
+    @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "employee_roles", joinColumns = @JoinColumn(name = "employee_id"))
     @Column(name = "role")
-    @ElementCollection(fetch = FetchType.EAGER)
-    @BatchSize(size = 200)
     private Set<Role> roles;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "employee")
     @OrderBy("date DESC")
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "employee")
     private List<EmployeePayment> payments;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.MERGE, mappedBy = "employee")
     @OrderBy("startDate DESC")
-    // @Fetch(value = FetchMode.SUBSELECT)
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.MERGE, mappedBy = "employee")
     private Set<Salary> salary;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.MERGE, mappedBy = "employee")
     @OrderBy("number DESC")
-    // @Fetch(value = FetchMode.SUBSELECT)
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.MERGE, mappedBy = "employee")
     private Set<Phone> phones;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.MERGE, mappedBy = "employee")
     @OrderBy("address DESC")
-    // @Fetch(value = FetchMode.SUBSELECT)
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.MERGE, mappedBy = "employee")
     private Set<Email> emails;
 
     public Employee() {
     }
 
-    public Employee(Integer id, String fullName, Region region, String login,
-                    String password, String address, Set<Role> roles, List<EmployeePayment> payments,
-                    Set<Salary> salary, Set<Phone> phones, Set<Email> emails) {
-        super(id, fullName);
-        this.region = region;
-        this.address = address;
-        this.login = login;
-        this.password = password;
-        this.roles = roles;
-        this.payments = payments;
-        this.salary = salary;
-        this.phones = phones;
-        this.emails = emails;
+    /**
+     * Cloning constructor
+     **/
+
+    public Employee(Employee other) {
+        super(other.getId(), other.getFullName());
+        this.region = other.getRegion();
+        this.address = other.getAddress();
+        this.login = other.getLogin();
+        this.password = other.getPassword();
+        this.roles = other.getRoles();
+        this.payments = other.getPayments();
+        this.salary = other.getSalary();
+        this.phones = other.getPhones();
+        this.emails = other.getEmails();
     }
 
     public Region getRegion() {
         return region;
     }
 
-    public void setRegion(Region region) {
-        this.region = region;
-    }
-
     public String getLogin() {
         return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getAddress() {
         return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
     public Set<Role> getRoles() {
         return roles;
     }
 
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
     public List<EmployeePayment> getPayments() {
         return payments;
-    }
-
-    public void setPayments(List<EmployeePayment> payments) {
-        this.payments = payments;
     }
 
     public Set<Salary> getSalary() {
         return salary;
     }
 
-    public void setSalary(Set<Salary> salary) {
-        this.salary = salary;
-    }
-
     public Set<Phone> getPhones() {
         return phones;
-    }
-
-    public void setPhones(Set<Phone> phones) {
-        this.phones = phones;
     }
 
     public Set<Email> getEmails() {
         return emails;
     }
 
+    public void setRegion(Region region) {
+        this.region = region;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public void setPayments(List<EmployeePayment> payments) {
+        this.payments = payments;
+    }
+
+    public void setSalary(Set<Salary> salary) {
+        this.salary = salary;
+    }
+
+    public void setPhones(Set<Phone> phones) {
+        this.phones = phones;
+    }
+
     public void setEmails(Set<Email> emails) {
         this.emails = emails;
     }
 
+    /**
+     * Fluent API
+     **/
+
+    public Employee id(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public Employee fullName(String fullName) {
+        this.fullName = fullName;
+        return this;
+    }
+
+    public Employee region(Region region) {
+        this.region = region;
+        return this;
+    }
+
+    public Employee login(String login) {
+        this.login = login;
+        return this;
+    }
+
+    public Employee password(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public Employee address(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Employee roles(Set<Role> roles) {
+        this.roles = roles;
+        return this;
+    }
+
+    public Employee payments(List<EmployeePayment> payments) {
+        this.payments = payments;
+        return this;
+    }
+
+    public Employee salary(Set<Salary> salary) {
+        this.salary = salary;
+        return this;
+    }
+
+    public Employee phones(Set<Phone> phones) {
+        this.phones = phones;
+        return this;
+    }
+
+    public Employee emails(Set<Email> emails) {
+        this.emails = emails;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        if (other == null || getClass() != other.getClass()) return false;
+        if (!super.equals(other)) return false;
+        Employee otherEmployee = (Employee) other;
+        return Objects.equals(login, otherEmployee.login) &&
+                Objects.equals(password, otherEmployee.password) &&
+                Objects.equals(address, otherEmployee.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), login, password, address);
+    }
+
     @Override
     public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", fullName='" + fullName + '\'' +
-                ", login='" + login + '\'' +
-                ", password='" + password + '\'' +
-                ", address='" + address + '\'' +
-                ", roles=" + roles +
-                '}';
+        return "Employee {" +
+                "id=" + id + ", " +
+                "fullName='" + fullName + ", " +
+                "login='" + login + ", " +
+                "password='" + password + ", " +
+                "address='" + address + ", " +
+                "roles=" + roles +
+                "}\n";
     }
+
 }
