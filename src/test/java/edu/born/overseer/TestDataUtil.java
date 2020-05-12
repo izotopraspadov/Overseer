2be0eb5851144fed28262b744ad597c760bdd5b7
izotopraspadov <izotopraspadov@gmail.com>
2020-05-12 18:02:11 +0300

    created TestDataUtil

---
+++ src/test/java/edu/born/overseer/TestDataUtil.java
@@ -0,0 +1,9 @@
+package edu.born.overseer;
+
+import static edu.born.overseer.model.abstraction.AbstractBaseEntity.START_SEQUENCE;
+
+public class TestDataUtil {
+
+    public static final int INVALID_ID = START_SEQUENCE - 1;
+
+}
