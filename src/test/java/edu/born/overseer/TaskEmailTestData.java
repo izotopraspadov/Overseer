57c0bb57d3be8b5d17a314c1c4f0cbe939c383e1
izotopraspadov <izotopraspadov@gmail.com>
2020-05-12 18:01:33 +0300

    TaskEmailTestData: used Fluent API

--- src/test/java/edu/born/overseer/TaskEmailTestData.java
+++ src/test/java/edu/born/overseer/TaskEmailTestData.java
@@ -9,58 +9,62 @@ import static edu.born.overseer.EmailTestData.*;
 import static edu.born.overseer.TaskTestData.*;
 import static edu.born.overseer.model.SendType.MANAGER;
 import static edu.born.overseer.model.SendType.TEAM_LEADER;
-import static edu.born.overseer.model.abstraction.AbstractBaseEntity.START_SEQUENCE;
 
 public class TaskEmailTestData {
 
-    public static final int INVALID_TASK_EMAIL_ID = START_SEQUENCE - 1;
-
     public static final Set<TaskEmail> TASK_1_EMAILS = Set.of(
-            new TaskEmail(TASK_1, CONTACT_PERSON_EMAIL_1, TEAM_LEADER),
-            new TaskEmail(TASK_1, EMPLOYEE_EMAIL_1, MANAGER)
+            new TaskEmail().task(TASK_1).email(CONTACT_PERSON_EMAIL_1).sendType(TEAM_LEADER),
+            new TaskEmail().task(TASK_1).email(EMPLOYEE_EMAIL_1).sendType(MANAGER)
     );
+
     public static final Set<TaskEmail> TASK_2_EMAILS = Set.of(
-            new TaskEmail(TASK_2, CONTACT_PERSON_EMAIL_2, TEAM_LEADER),
-            new TaskEmail(TASK_2, EMPLOYEE_EMAIL_2, MANAGER)
+            new TaskEmail().task(TASK_2).email(CONTACT_PERSON_EMAIL_2).sendType(TEAM_LEADER),
+            new TaskEmail().task(TASK_2).email(EMPLOYEE_EMAIL_2).sendType(MANAGER)
     );
+
     public static final Set<TaskEmail> TASK_3_EMAILS = Set.of(
-            new TaskEmail(TASK_3, CONTACT_PERSON_EMAIL_3, TEAM_LEADER),
-            new TaskEmail(TASK_3, EMPLOYEE_EMAIL_3, MANAGER)
+            new TaskEmail().task(TASK_3).email(CONTACT_PERSON_EMAIL_3).sendType(TEAM_LEADER),
+            new TaskEmail().task(TASK_3).email(EMPLOYEE_EMAIL_3).sendType(MANAGER)
     );
+
     public static final Set<TaskEmail> TASK_4_EMAILS = Set.of(
-            new TaskEmail(TASK_4, CONTACT_PERSON_EMAIL_4, TEAM_LEADER),
-            new TaskEmail(TASK_4, EMPLOYEE_EMAIL_4, MANAGER)
+            new TaskEmail().task(TASK_4).email(CONTACT_PERSON_EMAIL_4).sendType(TEAM_LEADER),
+            new TaskEmail().task(TASK_4).email(EMPLOYEE_EMAIL_4).sendType(MANAGER)
     );
+
     public static final Set<TaskEmail> TASK_5_EMAILS = Set.of(
-            new TaskEmail(TASK_5, CONTACT_PERSON_EMAIL_5, TEAM_LEADER),
-            new TaskEmail(TASK_5, CONTACT_PERSON_EMAIL_6, TEAM_LEADER),
-            new TaskEmail(TASK_5, CONTACT_PERSON_EMAIL_10, TEAM_LEADER),
-            new TaskEmail(TASK_5, EMPLOYEE_EMAIL_5, MANAGER)
+            new TaskEmail().task(TASK_5).email(CONTACT_PERSON_EMAIL_5).sendType(TEAM_LEADER),
+            new TaskEmail().task(TASK_5).email(CONTACT_PERSON_EMAIL_6).sendType(TEAM_LEADER),
+            new TaskEmail().task(TASK_5).email(CONTACT_PERSON_EMAIL_10).sendType(TEAM_LEADER),
+            new TaskEmail().task(TASK_5).email(EMPLOYEE_EMAIL_5).sendType(MANAGER)
     );
+
     public static final Set<TaskEmail> TASK_6_EMAILS = Set.of(
-            new TaskEmail(TASK_6, EMPLOYEE_EMAIL_6, TEAM_LEADER),
-            new TaskEmail(TASK_6, EMPLOYEE_EMAIL_7, MANAGER),
-            new TaskEmail(TASK_6, CONTACT_PERSON_EMAIL_11, MANAGER),
-            new TaskEmail(TASK_6, CONTACT_PERSON_EMAIL_12, MANAGER)
+            new TaskEmail().task(TASK_6).email(EMPLOYEE_EMAIL_6).sendType(TEAM_LEADER),
+            new TaskEmail().task(TASK_6).email(EMPLOYEE_EMAIL_7).sendType(MANAGER),
+            new TaskEmail().task(TASK_6).email(CONTACT_PERSON_EMAIL_11).sendType(MANAGER),
+            new TaskEmail().task(TASK_6).email(CONTACT_PERSON_EMAIL_12).sendType(MANAGER)
     );
+
     public static final Set<TaskEmail> TASK_7_EMAILS = Set.of(
-            new TaskEmail(TASK_7, CONTACT_PERSON_EMAIL_1, TEAM_LEADER),
-            new TaskEmail(TASK_7, CONTACT_PERSON_EMAIL_2, TEAM_LEADER),
-            new TaskEmail(TASK_7, CONTACT_PERSON_EMAIL_3, TEAM_LEADER),
-            new TaskEmail(TASK_7, CONTACT_PERSON_EMAIL_4, MANAGER),
-            new TaskEmail(TASK_7, CONTACT_PERSON_EMAIL_5, MANAGER),
-            new TaskEmail(TASK_7, CONTACT_PERSON_EMAIL_6, MANAGER)
+            new TaskEmail().task(TASK_7).email(CONTACT_PERSON_EMAIL_1).sendType(TEAM_LEADER),
+            new TaskEmail().task(TASK_7).email(CONTACT_PERSON_EMAIL_2).sendType(TEAM_LEADER),
+            new TaskEmail().task(TASK_7).email(CONTACT_PERSON_EMAIL_3).sendType(TEAM_LEADER),
+            new TaskEmail().task(TASK_7).email(CONTACT_PERSON_EMAIL_4).sendType(MANAGER),
+            new TaskEmail().task(TASK_7).email(CONTACT_PERSON_EMAIL_5).sendType(MANAGER),
+            new TaskEmail().task(TASK_7).email(CONTACT_PERSON_EMAIL_6).sendType(MANAGER)
     );
+
     public static final Set<TaskEmail> TASK_8_EMAILS = Set.of(
-            new TaskEmail(TASK_8, EMPLOYEE_EMAIL_1, TEAM_LEADER),
-            new TaskEmail(TASK_8, EMPLOYEE_EMAIL_2, TEAM_LEADER),
-            new TaskEmail(TASK_8, EMPLOYEE_EMAIL_3, TEAM_LEADER),
-            new TaskEmail(TASK_8, EMPLOYEE_EMAIL_4, MANAGER),
-            new TaskEmail(TASK_8, EMPLOYEE_EMAIL_5, MANAGER)
+            new TaskEmail().task(TASK_8).email(EMPLOYEE_EMAIL_1).sendType(TEAM_LEADER),
+            new TaskEmail().task(TASK_8).email(EMPLOYEE_EMAIL_2).sendType(TEAM_LEADER),
+            new TaskEmail().task(TASK_8).email(EMPLOYEE_EMAIL_3).sendType(TEAM_LEADER),
+            new TaskEmail().task(TASK_8).email(EMPLOYEE_EMAIL_4).sendType(MANAGER),
+            new TaskEmail().task(TASK_8).email(EMPLOYEE_EMAIL_5).sendType(MANAGER)
     );
 
     public static Set<TaskEmail> getPreparedCreateSet(Task task) {
-        return Set.of(new TaskEmail(task, EMPLOYEE_EMAIL_6, TEAM_LEADER));
+        return Set.of(new TaskEmail().task(task).email(EMPLOYEE_EMAIL_6).sendType(TEAM_LEADER));
     }
 
 }
