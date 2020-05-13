9b3ece339151d2d96bccbe72e1dc02c4cc9230fb
izotopraspadov <izotopraspadov@gmail.com>
2020-05-13 11:40:36 +0300

    CompanyTestData: used Fluent API

--- src/test/java/edu/born/overseer/CompanyTestData.java
+++ src/test/java/edu/born/overseer/CompanyTestData.java
@@ -9,30 +9,70 @@ import static edu.born.overseer.model.abstraction.AbstractBaseEntity.START_SEQUE
 
 public class CompanyTestData {
 
-    public static final int INVALID_COMPANY_ID = START_SEQUENCE - 1;
-
     public static final int COMPANY_1_ID = START_SEQUENCE + 13;
     public static final int COMPANY_2_ID = START_SEQUENCE + 14;
     public static final int COMPANY_3_ID = START_SEQUENCE + 15;
 
-    public static final Company COMPANY_1 = new Company(COMPANY_1_ID, "Первая Компания", REGION_1, "000000000000",
-            "Никакая ул. 1", LOW, "Первая", OUR);
-    public static final Company COMPANY_2 = new Company(COMPANY_2_ID, "Вторая Компания", REGION_2, "0000000001",
-            "Наша ул. 2", MIDDLE, "Вторая", CUSTOMER);
-    public static final Company COMPANY_3 = new Company(COMPANY_3_ID, "Третья Компания", REGION_13, "0000000002",
-            "Зимняя ул. 89", HIGH, "Третья", OTHER);
+    public static final String INVALID_ITN = "001";
+
+    public static final Company COMPANY_1 = new Company()
+            .id(COMPANY_1_ID)
+            .title("Первая Компания")
+            .region(REGION_1)
+            .itn("000000000000")
+            .address("Никакая ул. 1")
+            .reliabilityType(LOW)
+            .chatGroupName("Первая")
+            .companyType(OUR);
+
+    public static final Company COMPANY_2 = new Company()
+            .id(COMPANY_2_ID)
+            .title("Вторая Компания")
+            .region(REGION_2)
+            .itn("0000000001")
+            .address("Наша ул. 2")
+            .reliabilityType(MIDDLE)
+            .chatGroupName("Вторая")
+            .companyType(CUSTOMER);
+
+    public static final Company COMPANY_3 = new Company()
+            .id(COMPANY_3_ID)
+            .title("Третья Компания")
+            .region(REGION_13)
+            .itn("0000000002")
+            .address("Зимняя ул. 89")
+            .reliabilityType(HIGH)
+            .chatGroupName("Третья")
+            .companyType(OTHER);
 
     public static Company getPreparedCreate() {
-        return new Company(null, "New Company", REGION_2, "000000000003", "New Street", LOW, "New Group", OUR);
+
+        return new Company()
+                .title("New Company")
+                .region(REGION_2)
+                .itn("000000000003")
+                .address("New Street")
+                .reliabilityType(LOW)
+                .chatGroupName("New Group")
+                .companyType(OUR);
+    }
+
+    public static Company getPreparedCreateWithInvalidITN() {
+
+        return getPreparedCreate()
+                .itn(INVALID_ITN);
     }
 
     public static Company getPreparedDuplicate() {
-        return new Company(null, "New Company", REGION_2, COMPANY_1.getItn(), "New Street", LOW, "New Group", OUR);
+
+        return getPreparedCreate()
+                .itn(COMPANY_1.getItn());
     }
 
     public static Company getPreparedUpdate() {
-        return new Company(COMPANY_1.getId(), "Updated Company", COMPANY_1.getRegion(), COMPANY_1.getItn(),
-                COMPANY_1.getAddress(), COMPANY_1.getReliabilityType(), COMPANY_1.getChatGroupName(), COMPANY_1.getCompanyType());
+
+        return new Company(COMPANY_1)
+                .title("Updated Company");
     }
 
 }
