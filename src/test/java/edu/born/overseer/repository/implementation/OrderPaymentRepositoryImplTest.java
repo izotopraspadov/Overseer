3dc3f5aefaab9965b3c56d608d7383fc2ab04558
izotopraspadov <izotopraspadov@gmail.com>
2020-05-14 16:21:10 +0300

    created new empty test for repositories

---
+++ src/test/java/edu/born/overseer/repository/implementation/OrderPaymentRepositoryImplTest.java
@@ -0,0 +1,20 @@
+package edu.born.overseer.repository.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderPaymentRepositoryImplTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getAllByDate() {
+    }
+
+    @Test
+    void getAllByOrder() {
+    }
+}
