4d08bde3c578d91b0a780cf5cd2bf0b78fe36eb1
izotopraspadov <izotopraspadov@gmail.com>
2020-05-14 15:37:43 +0300

    RegionTestData: used Fluent API

--- src/test/java/edu/born/overseer/RegionTestData.java
+++ src/test/java/edu/born/overseer/RegionTestData.java
@@ -6,8 +6,6 @@ import static edu.born.overseer.model.abstraction.AbstractBaseEntity.START_SEQUE
 
 public class RegionTestData {
 
-    public static final int INVALID_REGION_ID = START_SEQUENCE - 1;
-
     public static final int REGION_1_ID = START_SEQUENCE;
     public static final int REGION_2_ID = START_SEQUENCE + 1;
     public static final int REGION_3_ID = START_SEQUENCE + 2;
@@ -22,30 +20,75 @@ public class RegionTestData {
     public static final int REGION_12_ID = START_SEQUENCE + 11;
     public static final int REGION_13_ID = START_SEQUENCE + 12;
 
-    public static final Region REGION_1 = new Region(REGION_1_ID, "Алтайский край");
-    public static final Region REGION_2 = new Region(REGION_2_ID, "Амурская область");
-    public static final Region REGION_3 = new Region(REGION_3_ID, "Владимирская область");
-    public static final Region REGION_4 = new Region(REGION_4_ID, "г. Москва");
-    public static final Region REGION_5 = new Region(REGION_5_ID, "Еврейская автономная область");
-    public static final Region REGION_6 = new Region(REGION_6_ID, "Ивановская область");
-    public static final Region REGION_7 = new Region(REGION_7_ID, "Камчатский край");
-    public static final Region REGION_8 = new Region(REGION_8_ID, "Ленинградская область");
-    public static final Region REGION_9 = new Region(REGION_9_ID, "Омская область");
-    public static final Region REGION_10 = new Region(REGION_10_ID, "Республика Алтай");
-    public static final Region REGION_11 = new Region(REGION_11_ID, "Самарская область");
-    public static final Region REGION_12 = new Region(REGION_12_ID, "Санкт-Петербург");
-    public static final Region REGION_13 = new Region(REGION_13_ID, "Ярославская область");
+    public static final Region REGION_1 = new Region()
+            .id(REGION_1_ID)
+            .title("Алтайский край");
+
+    public static final Region REGION_2 = new Region()
+            .id(REGION_2_ID)
+            .title("Амурская область");
+
+    public static final Region REGION_3 = new Region()
+            .id(REGION_3_ID)
+            .title("Владимирская область");
+
+    public static final Region REGION_4 = new Region()
+            .id(REGION_4_ID)
+            .title("г. Москва");
+
+    public static final Region REGION_5 = new Region()
+            .id(REGION_5_ID)
+            .title("Еврейская автономная область");
+
+    public static final Region REGION_6 = new Region()
+            .id(REGION_6_ID)
+            .title("Ивановская область");
+
+    public static final Region REGION_7 = new Region()
+            .id(REGION_7_ID)
+            .title("Камчатский край");
+
+    public static final Region REGION_8 = new Region()
+            .id(REGION_8_ID)
+            .title("Ленинградская область");
+
+    public static final Region REGION_9 = new Region()
+            .id(REGION_9_ID)
+            .title("Омская область");
+
+    public static final Region REGION_10 = new Region()
+            .id(REGION_10_ID)
+            .title("Республика Алтай");
+
+    public static final Region REGION_11 = new Region()
+            .id(REGION_11_ID)
+            .title("Самарская область");
+
+    public static final Region REGION_12 = new Region()
+            .id(REGION_12_ID)
+            .title("Санкт-Петербург");
+
+    public static final Region REGION_13 = new Region()
+            .id(REGION_13_ID)
+            .title("Ярославская область");
 
     public static Region getPreparedCreate() {
-        return new Region(null, "New region");
+
+        return new Region()
+                .title("New region");
     }
 
     public static Region getPreparedDuplicate() {
-        return new Region(null, REGION_1.getTitle());
+
+        return new Region()
+                .title(REGION_1.getTitle()); // duplicate
     }
 
     public static Region getPreparedUpdate() {
-        return new Region(REGION_1_ID, "Updated region");
+
+        return new Region()
+                .id(REGION_1_ID)
+                .title("Updated region"); // update
     }
 
 }
