179c17dfcb1a8b9cbf08027568250af6061db2e1
izotopraspadov <izotopraspadov@gmail.com>
2020-05-14 14:19:50 +0300

    EmployeePaymentTestData: used Fluent API

--- src/test/java/edu/born/overseer/EmployeePaymentTestData.java
+++ src/test/java/edu/born/overseer/EmployeePaymentTestData.java
@@ -1,5 +1,6 @@
 package edu.born.overseer;
 
+import edu.born.overseer.model.Employee;
 import edu.born.overseer.model.EmployeePayment;
 
 import java.math.BigDecimal;
@@ -14,8 +15,6 @@ import static edu.born.overseer.model.abstraction.AbstractBaseEntity.START_SEQUE
 
 public class EmployeePaymentTestData {
 
-    public static final int INVALID_EMPLOYEE_PAYMENT_ID = START_SEQUENCE - 1;
-
     public static final int EMPLOYEE_PAYMENT_1_ID = START_SEQUENCE + 79;
     public static final int EMPLOYEE_PAYMENT_2_ID = START_SEQUENCE + 80;
     public static final int EMPLOYEE_PAYMENT_3_ID = START_SEQUENCE + 81;
@@ -25,38 +24,81 @@ public class EmployeePaymentTestData {
     public static final int EMPLOYEE_PAYMENT_7_ID = START_SEQUENCE + 85;
     public static final int EMPLOYEE_PAYMENT_8_ID = START_SEQUENCE + 86;
 
-    public static final EmployeePayment EMPLOYEE_PAYMENT_1 =
-            new EmployeePayment(EMPLOYEE_PAYMENT_1_ID, LocalDate.of(2019, 10, 1),
-                    EMPLOYEE_1, COMPANY, COMPANY_1, null, BigDecimal.valueOf(10000.00),
-                    true, true, "$$$");
-    public static final EmployeePayment EMPLOYEE_PAYMENT_2 =
-            new EmployeePayment(EMPLOYEE_PAYMENT_2_ID, LocalDate.of(2019, 10, 1),
-                    EMPLOYEE_2, EMPLOYEE, null, EMPLOYEE_1, BigDecimal.valueOf(20000.00),
-                    false, false, "Give me %");
-    public static final EmployeePayment EMPLOYEE_PAYMENT_3 =
-            new EmployeePayment(EMPLOYEE_PAYMENT_3_ID, LocalDate.of(2019, 10, 2),
-                    EMPLOYEE_1, EMPLOYEE, null, EMPLOYEE_2, BigDecimal.valueOf(2000.00),
-                    false, false, null);
-    public static final EmployeePayment EMPLOYEE_PAYMENT_4 =
-            new EmployeePayment(EMPLOYEE_PAYMENT_4_ID, LocalDate.of(2019, 10, 2),
-                    EMPLOYEE_3, COMPANY, COMPANY_2, null, BigDecimal.valueOf(30000.00),
-                    true, false, null);
-    public static final EmployeePayment EMPLOYEE_PAYMENT_5 =
-            new EmployeePayment(EMPLOYEE_PAYMENT_5_ID, LocalDate.of(2019, 10, 3),
-                    EMPLOYEE_4, EMPLOYEE, null, EMPLOYEE_1, BigDecimal.valueOf(5000.00),
-                    false, true, "Good Job");
-    public static final EmployeePayment EMPLOYEE_PAYMENT_6 =
-            new EmployeePayment(EMPLOYEE_PAYMENT_6_ID, LocalDate.of(2019, 10, 3),
-                    EMPLOYEE_3, EMPLOYEE, null, EMPLOYEE_5, BigDecimal.valueOf(5000.00),
-                    false, true, "$$$");
-    public static final EmployeePayment EMPLOYEE_PAYMENT_7 =
-            new EmployeePayment(EMPLOYEE_PAYMENT_7_ID, LocalDate.of(2019, 10, 4),
-                    EMPLOYEE_4, COMPANY, COMPANY_3, null, BigDecimal.valueOf(7000.00),
-                    true, false, null);
-    public static final EmployeePayment EMPLOYEE_PAYMENT_8 =
-            new EmployeePayment(EMPLOYEE_PAYMENT_8_ID, LocalDate.of(2019, 10, 4),
-                    EMPLOYEE_5, COMPANY, COMPANY_3, null, BigDecimal.valueOf(9000.00),
-                    true, true, null);
+    public static final EmployeePayment EMPLOYEE_PAYMENT_1 = new EmployeePayment()
+            .id(EMPLOYEE_PAYMENT_1_ID)
+            .date(LocalDate.of(2019, 10, 1))
+            .transaction(BigDecimal.valueOf(10000.00))
+            .cashless(true)
+            .charge(true)
+            .employee(EMPLOYEE_1)
+            .counterpartyType(COMPANY)
+            .companyCounterparty(COMPANY_1)
+            .comment("$$$");
+
+    public static final EmployeePayment EMPLOYEE_PAYMENT_2 = new EmployeePayment()
+            .id(EMPLOYEE_PAYMENT_2_ID)
+            .date(LocalDate.of(2019, 10, 1))
+            .transaction(BigDecimal.valueOf(20000.00))
+            .employee(EMPLOYEE_2)
+            .counterpartyType(EMPLOYEE)
+            .employeeCounterparty(EMPLOYEE_1)
+            .comment("Give me %");
+
+    public static final EmployeePayment EMPLOYEE_PAYMENT_3 = new EmployeePayment()
+            .id(EMPLOYEE_PAYMENT_3_ID)
+            .date(LocalDate.of(2019, 10, 2))
+            .transaction(BigDecimal.valueOf(2000.00))
+            .employee(EMPLOYEE_1)
+            .counterpartyType(EMPLOYEE)
+            .employeeCounterparty(EMPLOYEE_2);
+
+    public static final EmployeePayment EMPLOYEE_PAYMENT_4 = new EmployeePayment()
+            .id(EMPLOYEE_PAYMENT_4_ID)
+            .date(LocalDate.of(2019, 10, 2))
+            .transaction(BigDecimal.valueOf(30000.00))
+            .cashless(true)
+            .employee(EMPLOYEE_3)
+            .counterpartyType(COMPANY)
+            .companyCounterparty(COMPANY_2);
+
+    public static final EmployeePayment EMPLOYEE_PAYMENT_5 = new EmployeePayment()
+            .id(EMPLOYEE_PAYMENT_5_ID)
+            .date(LocalDate.of(2019, 10, 3))
+            .transaction(BigDecimal.valueOf(5000.00))
+            .charge(true)
+            .employee(EMPLOYEE_4)
+            .counterpartyType(EMPLOYEE)
+            .employeeCounterparty(EMPLOYEE_1)
+            .comment("Good Job");
+
+    public static final EmployeePayment EMPLOYEE_PAYMENT_6 = new EmployeePayment()
+            .id(EMPLOYEE_PAYMENT_6_ID)
+            .date(LocalDate.of(2019, 10, 3))
+            .transaction(BigDecimal.valueOf(5000.00))
+            .charge(true)
+            .employee(EMPLOYEE_3)
+            .counterpartyType(EMPLOYEE)
+            .employeeCounterparty(EMPLOYEE_5)
+            .comment("$$$");
+
+    public static final EmployeePayment EMPLOYEE_PAYMENT_7 = new EmployeePayment()
+            .id(EMPLOYEE_PAYMENT_7_ID)
+            .date(LocalDate.of(2019, 10, 4))
+            .transaction(BigDecimal.valueOf(10000.00))
+            .cashless(true)
+            .employee(EMPLOYEE_4)
+            .counterpartyType(COMPANY)
+            .companyCounterparty(COMPANY_3);
+
+    public static final EmployeePayment EMPLOYEE_PAYMENT_8 = new EmployeePayment()
+            .id(EMPLOYEE_PAYMENT_8_ID)
+            .date(LocalDate.of(2019, 10, 4))
+            .transaction(BigDecimal.valueOf(9000.00))
+            .cashless(true)
+            .charge(true)
+            .employee(EMPLOYEE_5)
+            .counterpartyType(COMPANY)
+            .companyCounterparty(COMPANY_3);
 
     public static final List<EmployeePayment> EMPLOYEE_1_PAYMENTS = List.of(EMPLOYEE_PAYMENT_1, EMPLOYEE_PAYMENT_3);
     public static final List<EmployeePayment> EMPLOYEE_2_PAYMENTS = List.of(EMPLOYEE_PAYMENT_3);
@@ -64,5 +106,14 @@ public class EmployeePaymentTestData {
     public static final List<EmployeePayment> EMPLOYEE_4_PAYMENTS = List.of(EMPLOYEE_PAYMENT_5, EMPLOYEE_PAYMENT_7);
     public static final List<EmployeePayment> EMPLOYEE_5_PAYMENTS = List.of(EMPLOYEE_PAYMENT_8);
     public static final List<EmployeePayment> EMPLOYEE_6_PAYMENTS = List.of();
+
+    public static List<EmployeePayment> getPreparedCreateList(Employee employee) {
+        return List.of(new EmployeePayment()
+                .transaction(BigDecimal.valueOf(100000.00))
+                .charge(true)
+                .employee(EMPLOYEE_1)
+                .counterpartyType(COMPANY)
+                .companyCounterparty(COMPANY_1));
+    }
 
 }
