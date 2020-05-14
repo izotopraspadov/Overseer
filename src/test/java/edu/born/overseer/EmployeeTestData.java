173d82d02f444e4c5fef37c153e954dcf8347178
izotopraspadov <izotopraspadov@gmail.com>
2020-05-14 15:08:33 +0300

    EmployeeTestData: used Fluent API & added prepared methods

--- src/test/java/edu/born/overseer/EmployeeTestData.java
+++ src/test/java/edu/born/overseer/EmployeeTestData.java
@@ -1,9 +1,11 @@
 package edu.born.overseer;
 
+import edu.born.overseer.model.ContactPerson;
 import edu.born.overseer.model.Employee;
 
 import java.util.Set;
 
+import static edu.born.overseer.CompanyTestData.COMPANY_1;
 import static edu.born.overseer.EmailTestData.*;
 import static edu.born.overseer.EmployeePaymentTestData.*;
 import static edu.born.overseer.PhoneTestData.*;
@@ -15,8 +17,6 @@ import static edu.born.overseer.model.abstraction.AbstractBaseEntity.START_SEQUE
 
 public class EmployeeTestData {
 
-    public static final int INVALID_EMPLOYEE_ID = START_SEQUENCE - 1;
-
     public static final int EMPLOYEE_1_ID = START_SEQUENCE + 24;
     public static final int EMPLOYEE_2_ID = START_SEQUENCE + 25;
     public static final int EMPLOYEE_3_ID = START_SEQUENCE + 26;
@@ -24,23 +24,111 @@ public class EmployeeTestData {
     public static final int EMPLOYEE_5_ID = START_SEQUENCE + 28;
     public static final int EMPLOYEE_6_ID = START_SEQUENCE + 29;
 
-    public static final Employee EMPLOYEE_1 =
-            new Employee(EMPLOYEE_1_ID, "Романов Роман Романович", REGION_1, "user", "user", "Заставская ул. 6",
-                    Set.of(USER), EMPLOYEE_1_PAYMENTS, EMPLOYEE_1_SALARIES, EMPLOYEE_1_PHONES, EMPLOYEE_1_EMAILS);
-    public static final Employee EMPLOYEE_2 =
-            new Employee(EMPLOYEE_2_ID, "Степанов Степан Степанови", REGION_1, "stepanov", "user2", "Четвёртый пр. 10",
-                    Set.of(USER), EMPLOYEE_2_PAYMENTS, EMPLOYEE_2_SALARIES, EMPLOYEE_2_PHONES, EMPLOYEE_2_EMAILS);
-    public static final Employee EMPLOYEE_3 =
-            new Employee(EMPLOYEE_3_ID, "Романова Дарья Петровна", REGION_3, "romanova", "user3", "Пр. Отчаянных, 90",
-                    Set.of(USER), EMPLOYEE_3_PAYMENTS, EMPLOYEE_3_SALARIES, EMPLOYEE_3_PHONES, EMPLOYEE_3_EMAILS);
-    public static final Employee EMPLOYEE_4 =
-            new Employee(EMPLOYEE_4_ID, "Дмитриев Дмитрий Дмитриевич", REGION_4, "dmitriev", "user4", "Непростая ул. 7",
-                    Set.of(USER), EMPLOYEE_4_PAYMENTS, EMPLOYEE_4_SALARIES, EMPLOYEE_4_PHONES, EMPLOYEE_4_EMAILS);
-    public static final Employee EMPLOYEE_5 =
-            new Employee(EMPLOYEE_5_ID, "Максимов Максим Максимович", REGION_8, "admin", "admin", "Пр. Дно. 11",
-                    Set.of(USER, ADMIN), EMPLOYEE_5_PAYMENTS, EMPLOYEE_5_SALARIES, EMPLOYEE_5_PHONES, EMPLOYEE_5_EMAILS);
-    public static final Employee EMPLOYEE_6 =
-            new Employee(EMPLOYEE_6_ID, "Зайцева Мария Иосифовн", REGION_8, "zaiceva", "admi2", "Малая Опечаленная ул. 14",
-                    Set.of(USER, ADMIN), EMPLOYEE_6_PAYMENTS, EMPLOYEE_6_SALARIES, EMPLOYEE_6_PHONES, EMPLOYEE_6_EMAILS);
+    public static final Employee EMPLOYEE_1 = new Employee()
+            .id(EMPLOYEE_1_ID)
+            .fullName("Романов Роман Романович")
+            .region(REGION_1)
+            .login("user")
+            .password("user")
+            .address("Заставская ул. 6")
+            .roles(Set.of(USER))
+            .payments(EMPLOYEE_1_PAYMENTS)
+            .salary(EMPLOYEE_1_SALARIES)
+            .phones(EMPLOYEE_1_PHONES)
+            .emails(EMPLOYEE_1_EMAILS);
+
+    public static final Employee EMPLOYEE_2 = new Employee()
+            .id(EMPLOYEE_2_ID)
+            .fullName("Степанов Степан Степанович")
+            .region(REGION_1)
+            .login("stepanov")
+            .password("user2")
+            .address("Четвёртый пр. 10")
+            .roles(Set.of(USER))
+            .payments(EMPLOYEE_2_PAYMENTS)
+            .salary(EMPLOYEE_2_SALARIES)
+            .phones(EMPLOYEE_2_PHONES)
+            .emails(EMPLOYEE_2_EMAILS);
+
+    public static final Employee EMPLOYEE_3 = new Employee()
+            .id(EMPLOYEE_3_ID)
+            .fullName("Романова Дарья Петровна")
+            .region(REGION_3)
+            .login("romanova")
+            .password("user3")
+            .address("Пр. Отчаянных, 90")
+            .roles(Set.of(USER))
+            .payments(EMPLOYEE_3_PAYMENTS)
+            .salary(EMPLOYEE_3_SALARIES)
+            .phones(EMPLOYEE_3_PHONES)
+            .emails(EMPLOYEE_3_EMAILS);
+
+    public static final Employee EMPLOYEE_4 = new Employee()
+            .id(EMPLOYEE_4_ID)
+            .fullName("Дмитриев Дмитрий Дмитриевич")
+            .region(REGION_4)
+            .login("dmitriev")
+            .password("user4")
+            .address("Непростая ул. 7")
+            .roles(Set.of(USER))
+            .payments(EMPLOYEE_4_PAYMENTS)
+            .salary(EMPLOYEE_4_SALARIES)
+            .phones(EMPLOYEE_4_PHONES)
+            .emails(EMPLOYEE_4_EMAILS);
+
+    public static final Employee EMPLOYEE_5 = new Employee()
+            .id(EMPLOYEE_5_ID)
+            .fullName("Максимов Максим Максимович")
+            .region(REGION_8)
+            .login("admin")
+            .password("admin")
+            .address("Пр. Дно. 11")
+            .roles(Set.of(USER, ADMIN))
+            .payments(EMPLOYEE_5_PAYMENTS)
+            .salary(EMPLOYEE_5_SALARIES)
+            .phones(EMPLOYEE_5_PHONES)
+            .emails(EMPLOYEE_5_EMAILS);
+
+    public static final Employee EMPLOYEE_6 = new Employee()
+            .id(EMPLOYEE_6_ID)
+            .fullName("Зайцева Мария Иосифовна")
+            .region(REGION_8)
+            .login("zaiceva")
+            .password("admin2")
+            .address("Малая Опечаленная ул. 14")
+            .roles(Set.of(USER, ADMIN))
+            .payments(EMPLOYEE_6_PAYMENTS)
+            .salary(EMPLOYEE_6_SALARIES)
+            .phones(EMPLOYEE_6_PHONES)
+            .emails(EMPLOYEE_6_EMAILS);
+
+    public static Employee getPreparedCreate() {
+        var employee = new Employee()
+                .fullName("New Employee")
+                .region(REGION_1)
+                .login("new_login")
+                .password("new_password")
+                .address("New Address")
+                .roles(Set.of(USER));
+
+        employee.setPayments(EmployeePaymentTestData.getPreparedCreateSet(employee));
+        employee.setSalary(SalaryTestData.getPreparedCreatedSet(employee));
+        employee.setEmails(EmailTestData.getPreparedCreateSet(employee));
+        employee.setPhones(PhoneTestData.getPreparedCreatedSet(employee));
+
+        return employee;
+    }
+
+    public static Employee getPreparedDuplicateCreate() {
+
+        return getPreparedCreate()
+                .login(EMPLOYEE_1.getLogin()); // duplicate
+    }
+
+    public static Employee getPreparedUpdate() {
+
+        return new Employee(EMPLOYEE_1)
+                .fullName("Updated employee");
+    }
 
 }
