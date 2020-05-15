723b62624f94ac4ad6345dcd25420418d8fbabf4
izotopraspadov <izotopraspadov@gmail.com>
2020-05-15 18:07:04 +0300

    TestDataUtil: added constant NEXT_ID

--- src/test/java/edu/born/overseer/data/TestDataUtil.java
+++ src/test/java/edu/born/overseer/data/TestDataUtil.java
@@ -5,5 +5,6 @@ import static edu.born.overseer.model.abstraction.AbstractBaseEntity.START_SEQUE
 public class TestDataUtil {
 
     public static final int INVALID_ID = START_SEQUENCE - 1;
+    public static final int NEXT_ID = START_SEQUENCE + 131;
 
 }
