4dfc3b0c11d3aea1af30ff3fc7386389e3f5986c
izotopraspadov <izotopraspadov@gmail.com>
2020-05-18 16:11:57 +0300

    CompanyRestController: created

---
+++ src/main/java/edu/born/overseer/web/rest/company/CompanyRestController.java
@@ -0,0 +1,106 @@
+package edu.born.overseer.web.rest.company;
+
+import edu.born.overseer.model.Company;
+import edu.born.overseer.model.CompanyType;
+import edu.born.overseer.model.ReliabilityType;
+import edu.born.overseer.repository.CompanyRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = CompanyRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class CompanyRestController {
+
+    public static final String REST_URL = "/rest/companies";
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final CompanyRepository companyRepository;
+
+    public CompanyRestController(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Company create(@RequestBody Company company) {
+        int regionId = company.getRegion().getId();
+        log.info("create company {} by region {}", company, regionId);
+        return companyRepository.save(company, regionId);
+    }
+
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody Company company, @PathVariable int id) {
+        int regionId = company.getRegion().getId();
+        log.info("create company {} by region {}", company, regionId);
+        companyRepository.save(company, regionId);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public boolean delete(@PathVariable int id) {
+        log.info("delete company {}", id);
+        return companyRepository.delete(id);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Company getById(@PathVariable int id) {
+        log.info("get company {}", id);
+        return companyRepository.getById(id);
+    }
+
+    @GetMapping(params = {"contactPersonId"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Company getByContactPersonId(@RequestParam int contactPersonId) {
+        log.info("get company by contact person {}", contactPersonId);
+        return companyRepository.getByContactPersonId(contactPersonId);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Company> getAll() {
+        log.info("get all companies");
+        return companyRepository.getAll();
+    }
+
+    @GetMapping(params = {"regionId"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Company> getAllByRegion(@RequestParam int regionId) {
+        log.info("get all companies by region {}", regionId);
+        return companyRepository.getAllByRegion(regionId);
+    }
+
+    @GetMapping(params = {"reliability"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Company> getAllByReliability(@RequestParam ReliabilityType reliabilityType) {
+        log.info("get all companies by reliability {}", reliabilityType);
+        return companyRepository.getAllByReliability(reliabilityType);
+    }
+
+    @GetMapping(params = {"type"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Company> getAllByType(@RequestParam CompanyType type) {
+        log.info("get all companies by type company {}", type);
+        return companyRepository.getAllByType(type);
+    }
+
+    @GetMapping(params = {"title"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Company> getAllByTitle(@RequestParam String title) {
+        log.info("get all companies by title {}", title);
+        return companyRepository.getAllByTitle(title);
+    }
+
+    @GetMapping(params = {"address"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Company> getAllByAddress(@RequestParam String address) {
+        log.info("get all companies by address {}", address);
+        return companyRepository.getAllByAddress(address);
+    }
+
+    @GetMapping(params = {"itn"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Company> getAllByItb(@RequestParam String itn) {
+        log.info("get company by itn {}", itn);
+        return companyRepository.getAllByItb(itn);
+    }
+
+}
