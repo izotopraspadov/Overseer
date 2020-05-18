8cbb1df541355ae93701a2a57d9333892fa96d8d
izotopraspadov <izotopraspadov@gmail.com>
2020-05-18 16:29:08 +0300

    EmployeeRestController: created

---
+++ src/main/java/edu/born/overseer/web/rest/employee/EmployeeRestController.java
@@ -0,0 +1,86 @@
+package edu.born.overseer.web.rest.employee;
+
+import edu.born.overseer.model.Employee;
+import edu.born.overseer.repository.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = EmployeeRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class EmployeeRestController {
+
+    public static final String REST_URL = "/rest/employees";
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeRestController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Employee create(@RequestBody Employee employee) {
+        int regionId = employee.getRegion().getId();
+        log.info("create employee {} by region {}", employee, regionId);
+        return employeeRepository.save(employee, regionId);
+    }
+
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody Employee employee, @PathVariable int id) {
+        int regionId = employee.getRegion().getId();
+        log.info("update employee {} by region {}", employee, regionId);
+        employeeRepository.save(employee, regionId);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public boolean delete(@PathVariable int id) {
+        log.info("delete employee {}", id);
+        return employeeRepository.delete(id);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Employee getById(@PathVariable int id) {
+        log.info("get employee {}", id);
+        return employeeRepository.getById(id);
+    }
+
+    @GetMapping(params = {"login"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Employee getByLogin(@RequestParam String login) {
+        log.info("get all employees by login {}", login);
+        return employeeRepository.getByLogin(login);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Employee> getAll() {
+        log.info("get all employees");
+        return employeeRepository.getAll();
+    }
+
+    @GetMapping(params = {"regionId"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Employee> getAllByRegion(@RequestParam int regionId) {
+        log.info("get all employees by region {}", regionId);
+        return employeeRepository.getAllByRegion(regionId);
+    }
+
+    @GetMapping(params = {"address"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Employee> getAllByAddress(@RequestParam String address) {
+        log.info("get all employees by address {}", address);
+        return employeeRepository.getAllByAddress(address);
+    }
+
+    @GetMapping(params = {"fullName"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Employee> getAllByFullName(@RequestParam String fullName) {
+        log.info("get all employees by fullName {}", fullName);
+        return employeeRepository.getAllByFullName(fullName);
+    }
+
+}
