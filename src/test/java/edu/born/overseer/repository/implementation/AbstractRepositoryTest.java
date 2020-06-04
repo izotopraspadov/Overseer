a5b6c1fdf341ea85cf49d536bf893d4939de13e1
izotopraspadov <izotopraspadov@gmail.com>
2020-06-04 20:32:44 +0300

    created AbstractControllerTest, AbstractRepositoryTest

---
+++ src/test/java/edu/born/overseer/repository/implementation/AbstractRepositoryTest.java
@@ -0,0 +1,14 @@
+package edu.born.overseer.repository.implementation;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@SpringJUnitConfig(locations = {"classpath:spring/spring-db.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Sql(scripts = "classpath:db/population.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class AbstractRepositoryTest {
+
+}
