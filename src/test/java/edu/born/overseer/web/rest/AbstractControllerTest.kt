857cf76c283dc82592b72868ff5d5e256bdbe3f1
Sergey Simonov <welcometoriverwood@gmail.com>
2021-03-12 19:01:15 +0300

    convert AbstractControllerTest to kotlin

--- src/test/java/edu/born/overseer/web/rest/AbstractControllerTest.kt
+++ src/test/java/edu/born/overseer/web/rest/AbstractControllerTest.kt
@@ -1,47 +1,43 @@
-package edu.born.overseer.web.rest;
+package edu.born.overseer.web.rest
 
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
+import org.springframework.test.context.jdbc.Sql
+import org.springframework.test.context.jdbc.SqlConfig
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+import org.springframework.web.filter.CharacterEncodingFilter
+import javax.annotation.PostConstruct
 
-import javax.annotation.PostConstruct;
+@SpringJUnitWebConfig(locations = ["classpath:spring/spring-mvc.xml", "classpath:spring/spring-db.xml"])
+@RunWith(SpringJUnit4ClassRunner::class)
+@Sql(scripts = ["classpath:db/population.sql"], config = SqlConfig(encoding = "UTF-8"))
+abstract class AbstractControllerTest {
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-
-@SpringJUnitWebConfig(locations = {
-        "classpath:spring/spring-mvc.xml",
-        "classpath:spring/spring-db.xml"
-})
-@RunWith(SpringJUnit4ClassRunner.class)
-@Sql(scripts = "classpath:db/population.sql", config = @SqlConfig(encoding = "UTF-8"))
-abstract public class AbstractControllerTest {
-
-    private static final CharacterEncodingFilter CHARACTER_ENCODING_FILTER = new CharacterEncodingFilter();
-
-    static {
-        CHARACTER_ENCODING_FILTER.setEncoding("UTF-8");
-        CHARACTER_ENCODING_FILTER.setForceEncoding(true);
+    companion object {
+        private val CHARACTER_ENCODING_FILTER: CharacterEncodingFilter =
+                CharacterEncodingFilter().apply {
+                    encoding = "UTF-8"
+                    setForceEncoding(true)
+                }
     }
 
-    protected MockMvc mockMvc;
+    protected lateinit var mockMvc: MockMvc
 
     @Autowired
-    protected WebApplicationContext webApplicationContext;
+    protected lateinit var webApplicationContext: WebApplicationContext
 
     @PostConstruct
-    private void postConstruct() {
+    private fun postConstruct() {
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(webApplicationContext)
-                .addFilter(CHARACTER_ENCODING_FILTER)
-                .apply(springSecurity())
-                .build();
+                .addFilter<DefaultMockMvcBuilder>(CHARACTER_ENCODING_FILTER)
+                .apply<DefaultMockMvcBuilder>(SecurityMockMvcConfigurers.springSecurity())
+                .build()
     }
-
 }
